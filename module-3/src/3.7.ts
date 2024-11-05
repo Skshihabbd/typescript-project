ad64a253db9719d541c548ce3ceea49bf982fba4
unknown <skshihab170@gmail.com>
2024-11-05 16:16:18 +0600

    static  oop

---
+++ module-3/src/3.7.ts
@@ -0,0 +1,63 @@
+{
+  //statics in OOP
+  class Counter {
+    count: number = 0;
+    increment() {
+      return (this.count = this.count + 1);
+    }
+    decrement() {
+      return (this.count = this.count - 1);
+    }
+  }
+
+  //   আলাদা আলাদা memory তে জমা হয় তার জন্য  প্রতিবার  আলাদা ভাবে variable এ declare করলে নতুন করে শুরু করে value দেই। উপরের console লগ এর উপর নির্ভর করেনা যে ওটাতে কি পরিবর্তন হল তার উপর পরবর্তী তে বাড়াবে নাকি কমাবে value.
+  const instance1 = new Counter();
+  console.log(instance1.increment()); //1->>different memory
+  console.log(instance1.increment()); //2->>different memory
+  console.log(instance1.increment()); //3->>different memory
+  const instance2 = new Counter();
+  console.log(instance2.increment()); // 1->>different memory
+  console.log(instance2.increment()); // 2->>different memory
+
+  //
+  {
+    class Counter {
+      static count: number = 0;
+      increment() {
+        return (Counter.count = Counter.count + 1);
+      }
+      decrement() {
+        return (Counter.count = Counter.count - 1);
+      }
+    }
+    const instance1 = new Counter();
+    console.log(instance1.increment()); //1->>this time store same memory for using static memory
+    console.log(instance1.increment()); //2->>this time store same memory for using static memory
+    console.log(instance1.increment()); //3->>this time store same memory for using static memory
+    const instance2 = new Counter();
+    console.log(instance2.increment()); // 4->>this time store same memory for using static memory
+    console.log(instance2.increment()); // 5->>this time store same memory for using static memory
+  }
+  //
+  {
+    // make this method static
+    class Counter {
+      static count: number = 0;
+      static increment() {
+        return (Counter.count = Counter.count + 1);
+      }
+      static decrement() {
+        return (Counter.count = Counter.count - 1);
+      }
+    }
+    //const instance1 = new Counter();
+    console.log(Counter.increment()); //1->>this time store same memory for using static memory
+    console.log(Counter.increment()); //2->>this time store same memory for using static memory
+    console.log(Counter.increment()); //3->>this time store same memory for using static memory
+    //const instance2 = new Counter();
+    console.log(Counter.increment()); // 4->>this time store same memory for using static memory
+    console.log(Counter.increment()); // 5->>this time store same memory for using static memory
+  }
+
+  //
+}
